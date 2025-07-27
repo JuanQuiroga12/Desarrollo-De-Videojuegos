68b2678a1440f49f9bdbae45f02ee8fe5ac8afee
JuanQuiroga12 <est.juand.quiroga@unimilitar.edu.co>
2025-07-27 01:23:41 -0500

    Commit 3 Juan Quiroga

--- Assets/Scripts/Commits.cs
+++ Assets/Scripts/Commits.cs
@@ -2,3 +2,4 @@
 // 27/07/2025 - 1:10 am - Ana Sofia Gomez Sanabria
 // 27/07/2025 - 1:18 am - Juan David Quiroga González
 // 27/07/2025 - 1:22 am - Ana Sofia Gomez Sanabria
+// 27/07/2025 - 1:23 am - Juan David Quiroga González
