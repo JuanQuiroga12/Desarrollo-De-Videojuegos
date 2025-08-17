c27637975f9b83b2a48b41faea7a01f6e7879f57
JuanQuiroga12 <est.juand.quiroga@unimilitar.edu.co>
2025-08-16 21:18:08 -0500

    4 primero botones funcionales

--- Assets/Scripts/Controles.cs
+++ Assets/Scripts/Controles.cs
@@ -3,15 +3,40 @@ using UnityEngine;
 public class Controles : MonoBehaviour
 {
     public GameObject Rabbit;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public void RotateLeft()
     {
-        
+        Rabbit.transform.Rotate(0.0f, 10.0f, 0.0f,Space.Self);
+    }
+    public void RotateRight()
+    {
+        Rabbit.transform.Rotate(0.0f, -10.0f, 0.0f, Space.Self);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TranslateUp()
     {
-        
+        Rabbit.transform.Translate(Vector3.up * Time.deltaTime * 10, Space.World);
+    }
+
+    public void TranslateDown()
+    {
+        Rabbit.transform.Translate(Vector3.down * Time.deltaTime * 10, Space.World);
+    }
+
+    public void TranslateLeft()
+    {
+        Rabbit.transform.Translate(Vector3.left * Time.deltaTime * 10, Space.World);
+    }
+
+    public void TranslateRight()
+    {
+        Rabbit.transform.Translate(Vector3.right * Time.deltaTime * 10, Space.World);
+    }
+
+    public void Scale(float magnitud)
+    {
+        Vector3 changerscale = new Vector3(magnitud, magnitud, magnitud);
+
+        Rabbit.transform.localScale += changerscale;
     }
 }
